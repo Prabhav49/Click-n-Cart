a09e9af010b5c3c0fa7974b82106a4ae92dc0594
Prabhav <iamprabhav23@gmail.com>
2023-07-04 23:01:43 +0530

    Created Login API and added JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import userModel from '../models/userModel.js'
-import { hashPassword} from './../helpers/authHelper.js';
+import { comparePassword, hashPassword} from './../helpers/authHelper.js';
+import JWT from 'jsonwebtoken';
 
 export const registerController = async (req,res) => {
     try {
@@ -58,5 +59,63 @@ export const registerController = async (req,res) => {
             error
         })
     }
-}
+};
 
+//POST LOGIN
+export const loginController = async(req,res) =>{
+    try {
+        const {email,password} = req.body
+        //Validation
+        if(!email || !password){
+            return res.status(404).send({
+                success:false,
+                message:"Invalid Email or Password"
+            })
+        }
+        //check user
+        const user = await userModel.findOne({email})
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message:'Email is not registered'
+            })
+        }
+
+        const match = await comparePassword(password, user.password)
+        if(!match){
+            return res.status(200).send({
+                success:false,
+                message:'Invalid Password'
+            })
+        }
+        //Token
+        const token = await JWT.sign({_id:user._id}, process.env.JWT_SECRET, {
+            expiresIn:'7d'
+        });
+
+        res.status(200).send({
+            success:true,
+            message:'Login Successfully',
+            user:{
+                name:user.name,
+                email:user.email,
+                phone:user.phone,
+                address:user.address
+            },
+            token,
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message: ' Error in Login',
+            error
+        })
+    }
+};
+
+//Test Controller 
+export const testController = (req,res) => {
+    res.send('Protected Route')
+}
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,17 @@
 import express from "express";
-import {registerController} from '../controllers/authController.js'
-
+import {loginController,registerController,testController} from '../controllers/authController.js'
+import { requireSignIn } from "../middlewares/authMiddleware.js";
 //router object
 const router = express.Router()
 
 //routing
 //Register
 router.post("/register", registerController)
+
+//Login
+router.post('/login', loginController)
+
+//Test Routes
+router.get('/test',requireSignIn,testController)
 
 export default router
